5fe47cee8ab28f1a10d2d1bbba56075bf3761cb3
Sergio Uve <sergiouve@gmail.com>
2019-01-28 19:11:41 +0100

    Add Day 1, Part 2 solution

--- 01_chronal_calibration/solution.py
+++ 01_chronal_calibration/solution.py
@@ -1,28 +1,52 @@
-def parseInput():
+import sys
+import collections
+
+
+from functools import reduce
+
+def parse_input():
     input = open("input", "r")
-    frequenciesList = input.read().splitlines()
+    frequencies_list = input.read().splitlines()
 
-    return frequenciesList
+    return frequencies_list
 
-def formatFrequencies(frequenciesList):
-    for position in range(len(frequenciesList)):
-        frequenciesList[position] = int(frequenciesList[position])
 
-    return frequenciesList
+def format_frequencies(frequencies_list):
+    for position in range(len(frequencies_list)):
+        frequencies_list[position] = int(frequencies_list[position])
+
+    return frequencies_list
+
 
 def calculateAdjustedFrequency(frequenciesList):
+    return reduce((lambda x, y: x + y), frequenciesList)
+
+
+def get_first_repeated_frequency(frequencies_list):
     frequency = 0
+    repeated = 0
+    seen_frequencies = []
 
-    for offset in frequenciesList:
-        frequency += offset
+    for offset in frequencies_list:
+        frequency += int(offset)
+        print(frequency)
 
-    return frequency
+        if frequency in seen_frequencies:
+            repeated = frequency
+            break
+        else:
+            seen_frequencies.append(frequency)
+
+    return repeated
 
 
 def main():
-    formattedInput = formatFrequencies(parseInput())
+    formattedInput = format_frequencies(parse_input())
     adjustedFrequency = calculateAdjustedFrequency(formattedInput)
+    firstRepeatedFrequency = get_first_repeated_frequency(formattedInput)
 
-    print adjustedFrequency
+    print(f'resulting_frequency: {adjustedFrequency}')
+    print(f'first_repeated_frequency: {firstRepeatedFrequency}')
+    sys.exit()
 
 main()
